0e4400f30809bcf50feb6a5cd373227a95e3bca4
allrainvot <zhouyunlu0216@gmail.com>
2016-11-28 14:24:54 -0500

    add user login

--- src/java/com/smartFarm/controller/loginController.java
+++ src/java/com/smartFarm/controller/loginController.java
@@ -11,6 +11,7 @@ import com.smartFarm.pojo.Doctor;
 import com.smartFarm.pojo.Employee;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/login.htm")
 public class loginController {
     
+    @Autowired
     userDao userDao;
     
     protected String login(HttpServletRequest request, Model model) throws Exception{
