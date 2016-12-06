1fe69cb9593e6a9e01ef581878eba66193763cfe
Jing Li <li.jing6>
2016-12-05 23:01:12 -0500

    add timer ends

--- src/java/com/smartFarm/controller/SensorController.java
+++ src/java/com/smartFarm/controller/SensorController.java
@@ -5,7 +5,10 @@
  */
 package com.smartFarm.controller;
 
+import com.smartFarm.DAO.AccelerationSensorDao;
+import com.smartFarm.DAO.MilkSensorDao;
 import com.smartFarm.DAO.TempSensorDao;
+import com.smartFarm.DAO.WeightSensorDao;
 import java.util.Timer;
 import java.util.TimerTask;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,11 +27,23 @@ public class SensorController {
 
     @Autowired
     TempSensorDao tsd = new TempSensorDao();
+    
+    @Autowired
+    WeightSensorDao wsd = new WeightSensorDao();
+    
+    @Autowired
+    AccelerationSensorDao asd = new AccelerationSensorDao();
+    
+    @Autowired
+    MilkSensorDao msd = new MilkSensorDao();
 
     //need map
     @RequestMapping(value = "/startsensing.htm", method = RequestMethod.GET)
     public String beginTempSensing() {
 
+        wsd.sensing();
+        asd.sensing();
+        msd.sensing();
         tsd.sensing();
         return "sensing";
     }
@@ -37,7 +52,11 @@ public class SensorController {
     @RequestMapping(value = "/endsensing.htm", method = RequestMethod.GET)
     public String stopTempSensing() {
 
+        wsd.stopSensing();
+        asd.stopSensing();
+        msd.stopSensing();
         tsd.stopSensing();
+        
         return "sensing";
     }
 
